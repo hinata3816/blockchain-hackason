d60a245ee0229ef69ce15626977131536041760b
wuhe <wuhe52@qq.com>
2019-02-15 18:51:55 +0800

    debug

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,18 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+    this.setRoles(userInfo.identity)
+  },
+  methods: {
+    ...mapMutations({
+      setRoles: 'SET_ROLES'
+    })
+  }
 }
 </script>
